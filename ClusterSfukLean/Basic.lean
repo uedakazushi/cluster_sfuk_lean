effdb68a8b679371b1f560d0af337eb72d8a1007
Kazushi Ueda <kazushi@ms.u-tokyo.ac.jp>
2024-07-20 07:57:07 +0900

    edit

--- ClusterSfukLean/Basic.lean
+++ ClusterSfukLean/Basic.lean
@@ -1,32 +1,65 @@
-import Mathlib.Init.Set
-import Mathlib.Data.Set.Finite
-
-#eval 7/3
-#eval 8%3
-
-set_option diagnostics true
-
-def set1 : Set Nat := {n : Nat | n < 10}
-
-#check set1
-
-#check Set.finite_le_nat
+import Mathlib
 
 lemma finite_test (k:Nat) : Set.Finite {n : Nat | n ≤ k } := by
   exact Set.finite_le_nat k
 
-def I (e f i : Nat) : Set Nat :=
-  {n : Nat |
+#check Set.Finite.subset
+
+lemma finite_of_bounded_of_Nat (s: Set ℕ) :
+  ∃ k : ℕ, ∀ x ∈ s, x ≤ k → s.Finite := by
+  sorry
+
+section
+
+variable (e f : ℕ+)
+variable (i : ℕ)
+
+def setI : Set ℕ :=
+  {n : ℕ |
   n % e ≠ e - 1
   ∧ n % f ≠ f - 1
   ∧ n / e + n / f = i }
 
-theorem bdd_test (e f n i: Nat) :
-  n / e + n / f = i → n ≤ e * i + e := by
-  intro h
-  have h1 : n / e ≤ i := Nat.le.intro h
-  have h2 := Nat.mul_le_mul_left e h1
-  have h3 : e * (n / e) + n % e = n := by
-    rw [Nat.div_add_mod]
-  have h4 := Nat.le.intro h3
+def setII : Set ℕ :=
+  {n : ℕ |
+  n % e = e - 1
+  ∧ n % f = f - 1
+  ∧ n / e + n / f + 1 = i }
+
+lemma setI_finite : Set.Finite (setI e f i) := by
+  have setI_bdd : (setI e f i) ⊆ {n : ℕ | n ≤ e * i + e} := by
+    rw [Set.subset_def]
+    intro x
+    intro h
+    simp [setI] at h
+    have h1 : x / ↑e + x / ↑f = i := h.right.right
+    simp
+    have h2 : x / ↑e ≤ i := by sorry
+    sorry
+
+lemma setII_finite : Set.Finite (setII e f i) := by
   sorry
+
+noncomputable def finsetI : Finset ℕ := Set.Finite.toFinset (setI_finite e f i)
+
+noncomputable def finsetII : Finset ℕ := Set.Finite.toFinset (setII_finite e f i)
+
+noncomputable def cardI : ℕ := (finsetI e f i).card
+
+noncomputable def cardII : ℕ := (finsetII e f i).card
+
+noncomputable def h : ℕ := (cardI e f i) + (cardII e f i)
+
+end
+
+section
+
+variable (e f l: ℕ+)
+variable (i : ℕ)
+variable (e_coprime_f : Nat.Coprime e f)
+
+theorem main :
+  h (e * l) (f * l) i = l * (h e f i) + l - 1
+  := by
+  sorry
+end
