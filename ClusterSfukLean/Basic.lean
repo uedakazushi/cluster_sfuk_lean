2e98f26ad712b27d5ef8b02b8ded5a0a8b8059f6
Kazushi Ueda <kazushi@ms.u-tokyo.ac.jp>
2024-07-21 07:29:43 +0900

    edit

--- ClusterSfukLean/Basic.lean
+++ ClusterSfukLean/Basic.lean
@@ -1,10 +1,5 @@
 import Mathlib
 
-lemma finite_test (k:Nat) : Set.Finite {n : Nat | n ≤ k } := by
-  exact Set.finite_le_nat k
-
-#check Set.Finite.subset
-
 lemma finite_of_bounded_of_Nat (s: Set ℕ) :
   (∃ k : ℕ, ∀ x ∈ s, x ≤ k) → s.Finite := by
   intro h
@@ -17,6 +12,41 @@ lemma finite_of_bounded_of_Nat (s: Set ℕ) :
         exact h x h2
       apply Set.Finite.subset (Set.finite_le_nat k)
       assumption
+
+lemma nat_mod_pnat_le (n : ℕ) (m : ℕ+) : n % m ≤ m := by
+  cases n with
+  | zero => simp
+  | succ n' =>
+      let n : PNat := ⟨ Nat.succ n', Nat.succ_pos n' ⟩
+      have nn' : n'+1 = n := rfl
+      have h1 := (PNat.mod_le n m).2
+      have h2 := PNat.mod_coe n m
+      rw [nn']
+      cases (n.1) % m with
+      | zero =>
+          split_ifs at h2
+          · linarith
+          · rw [←h2]
+            simp
+            exact h1
+      | succ k =>
+          split_ifs at h2
+          · linarith
+          · rw [←h2]
+            simp
+            exact h1
+
+lemma nat_div_pnat_le (n q : ℕ) (d : ℕ+) : n / d ≤ q → n ≤ d * q + d := by
+  intro h1
+  have h2 : n = d * (n / d) + n % d := Eq.symm (Nat.div_add_mod n d)
+  have h3 := nat_mod_pnat_le n d
+  have h5 : d * (n/d) ≤ d * q := by
+    apply mul_le_mul
+    apply le_refl
+    assumption
+    apply Nat.zero_le
+    apply Nat.zero_le
+  linarith
 
 section
 
@@ -38,62 +68,29 @@ def setII : Set ℕ :=
 lemma Nat_le_add_right (a b : ℕ) : a ≤ a + b := by
   linarith
 
--- example (m: ℕ) (n d : ℕ+) : n / d ≤ m → n ≤ d * m + d := by
---   intro h1
---   #check (↑d : Nat)
---   have h2 : n = d * ((n:Nat) / d) + n % d := Eq.symm (Nat.div_add_mod n d)
---   have h3 := Nat.mul_le_mul_left d h1
---   have h4 := Nat.add_le_add_right h3 (n % d)
---   have h5 : n ≤ d * m + ↑n % d := by linarith
---   have h6 := (PNat.mod_le n d).2
---   have h7 : n ≤ d -> (n:Nat) ≤ (d:Nat) := by
---     intro h
---     exact h
---   have h8 : (n: Nat) % d = (n.mod d : Nat) := by
---     induction n
---     ·
+lemma setI_finite : Set.Finite (setI e f i) := by
+  have setI_bdd : ∃ k : ℕ, ∀ n ∈ setI e f i, n ≤ k := by
+    exists e*i+e
+    intro n
+    dsimp [setI]
+    intro h
+    cases h with
+    | intro h1 h2 => cases h2 with
+    | intro h2 h3 =>
+    have h4 : n/e ≤ i := by
+      have h5 : n / e ≤ n/e+n/f := by
+        apply Nat_le_add_right
+      linarith
+    have h6 := nat_div_pnat_le n i e
+    exact h6 h4
+  apply finite_of_bounded_of_Nat
+  assumption
 
---   have h8 : (n: Nat) % (↑d) ≤ d :=
+noncomputable def finsetI : Finset ℕ := Set.Finite.toFinset (setI_finite e f i)
 
+noncomputable def cardI : ℕ := (finsetI e f i).card
 
-
---     _ ≤ d * m + d := by
---       apply Nat.add_le_add_left
---       exact PNat.mod_le n d
-
-
-    -- ... ≤ i * m + m : by
-    --   intro h
-    --   have h1 : x / m * m ≤ i * m := Nat.mul_le_mul_left m h
-    --   have h2 : x % m ≤ m := Nat.mod_le x m
-    --   linarith
-
-
--- lemma setI_finite : Set.Finite (setI e f i) := by
---   have setI_bdd : (setI e f i) ⊆ {n : ℕ | n ≤ e * i + e} := by
---     rw [Set.subset_def]
---     intro x
---     intro h
---     simp [setI] at h
---     have h1 : x / ↑e + x / ↑f = i := h.right.right
---     simp
---     have h2 : x / ↑e ≤ i := by
---       have h3 := Nat_le_add_right (x / ↑e) (x / ↑f)
---       rw [h1] at h3
---       assumption
-
--- lemma setII_finite : Set.Finite (setII e f i) := by
---   sorry
-
--- noncomputable def finsetI : Finset ℕ := Set.Finite.toFinset (setI_finite e f i)
-
--- noncomputable def finsetII : Finset ℕ := Set.Finite.toFinset (setII_finite e f i)
-
--- noncomputable def cardI : ℕ := (finsetI e f i).card
-
--- noncomputable def cardII : ℕ := (finsetII e f i).card
-
--- noncomputable def h : ℕ := (cardI e f i) + (cardII e f i)
+noncomputable def h : ℕ := (cardI e f i)
 
 end
 
@@ -103,8 +100,8 @@ variable (e f l: ℕ+)
 variable (i : ℕ)
 variable (e_coprime_f : Nat.Coprime e f)
 
--- theorem main :
---   h (e * l) (f * l) i = l * (h e f i) + l - 1
---   := by
---   sorry
--- end
+theorem main :
+  h (e * l) (f * l) i = l * (h e f i) + l - 1
+  := by
+  sorry
+end
